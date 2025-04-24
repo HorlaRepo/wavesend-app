38cc339b2db7bbfddbc628116642cba31f3ce8f2
Francis Oladosu <32479488+HorlaRepo@users.noreply.github.com>
2025-04-24 12:44:17 +0100

    security updates 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.wavesend.cc', 
+  apiUrl: 'https://api.wavesend.com',
   keycloak: {
-    url: 'https://auth.wavesend.cc',
+    url: 'https://wavesend-keycloak-app-fpbkevc2fufed4a6.eastus2-01.azurewebsites.net',
     realm: 'wavesend',
     clientId: 'wavesend-frontend'
   }
