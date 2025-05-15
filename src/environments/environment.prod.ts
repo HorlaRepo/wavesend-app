1f9ed420d0a58458253a5dabf9f2dd66d1fc68fe
Francis Oladosu <32479488+HorlaRepo@users.noreply.github.com>
2025-05-15 20:13:07 +0100

    changes made to connect to google cloud

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.wavesend.com',
   keycloak: {
-    url: 'https://wavesend-keycloak-app-fpbkevc2fufed4a6.eastus2-01.azurewebsites.net',
+    url: 'https://auth.wavesend.cc',
     realm: 'wavesend',
     clientId: 'wavesend-frontend'
   },
-  stripePublishableKey: 'pk_live_51OaEANBL7OKV3JroSHgD9lM32OpfHiApX3s3NT7M56iotLkOdTKWbnd9dXyzGVLcLubO0tRIc6aj9yy8EnWQh7Uv00EXcZTLJu'
+  stripePublishableKey: 'pk_test_51OaEANBL7OKV3JrokH1Emj8BmIXB9suTSQUoVXJglIUJPsl4dpe1rTgYn4coKb5He89WcU3JK2ILrrH1IkQZ1wMf006DlQnQ57'
 };
